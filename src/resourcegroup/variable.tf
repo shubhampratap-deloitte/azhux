77b689f64d165d153ad4f924f48dd9b3d72cd5bd
shubhamprataps <90683007+shubhamprataps@users.noreply.github.com>
2021-09-15 22:14:49 +0530

    Delete variable.tf

--- src/resourcegroup/variable.tf
+++
@@ -1,7 +0,0 @@
-variable "location" {
-    description = "The Azure Region in which the Resource Group exists"
-}
-
-variable "rg_name" {
-    description = "The name of the Resource Group where VM resources will be created"
-}
